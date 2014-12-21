044ee147064501f5a7028d8649e4aee10be59c44
hshapiro <hnrshapiro@gmail.com>
2014-12-21 13:42:38 -0800

    Initial commit for cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## I store the function in a variable called makeCacheMatrix, which creates
+## a list to set + get the value of the matrix and to set + get the value
+## of the matrix's inverse. In this case, m is the value returned from the 
+## function applied to the matrix that will be cached and retrieved by the 
+## next function.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+  }
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list (set=set, get=get,
+      setmatrix=setmatrix,
+      getmatrix=getmatrix)
 }
 
+## I then store the cacheSolve function in a variable called cacheSolve.
+## In here, I can retrieve the prior value of m by calling getmatrix from 
+## the makeCacheMatrix function. Effectively, to run this,
+## I will call cacheSolve(makeCacheMatrix) after storing
+## the matrix. If m isn't null, then it will return m (assigned to the 
+## other environment in makeCacheMatrix).
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data -- hold on")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m
 }
